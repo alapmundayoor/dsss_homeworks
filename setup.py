1d3400d271b0e700c2c1525ba23215feed40386f
alapmundayoor <alapmundayoor@gmail.com>
2023-11-13 22:30:16 +0100

    Latest setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
-# from setuptools import setup, find_packages
+from setuptools import setup
 
-# setup(
-#     name='math_quiz',
-#     version='0.1',
-#     packages=find_packages(),
-#     install_requires=[
-#         'random',
-#     ],
-#     entry_points={
-#         'console_scripts': [
-#             'math_quiz = math_quiz.math_quiz:main',
-#         ],
-#     },
-# )
+setup(
+    name='math_quiz',
+    version='1.0.0',    
+    description='A math quiz',
+    author='Alap Mundayoor',
+    license='Apache',
+    packages=['math_quiz'],
+    install_requires=['random', 'unittest'                    
+                      ],
+
+    classifiers=[
+        'Development Status :: Game',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache-2.0 license', 
+    ],
+)
